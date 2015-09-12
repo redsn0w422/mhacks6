aeb09b2473f306d295fd4aa33225af1bb34e67da
Niraj Pant <nirajpant7@gmail.com>
2015-09-12 15:26:11 -0400

    IT WORKS

--- Bought/Bought/ViewController.swift
+++ Bought/Bought/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Bought
-//
-//  Created by Yasha Mostofi on 9/12/15.
-//  Copyright (c) 2015 Yasha Mostofi. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
--- Bought/Bought/WebViewController.swift
+++ Bought/Bought/WebViewController.swift
@@ -1,11 +1,3 @@
-//
-//  WebViewController.swift
-//  Bought
-//
-//  Created by Niraj Pant on 9/12/15.
-//  Copyright (c) 2015 Yasha Mostofi. All rights reserved.
-//
-
 import Foundation
 import UIKit
 import Firebase
@@ -13,14 +5,19 @@ import Firebase
 class WebViewController: UIViewController {
     var myRootRef = Firebase(url:"https://bought.firebaseio.com")
     var url:String?
-
+    var product : [AnyObject] = []
+//    var product : [NSMutableDictionary] = []
+    // Define UI elements
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var dollarAmountField: UITextField!
+    
     @IBAction func addProductButton(sender: AnyObject) {
-        
+        product.append(dollarAmountField.text)
+        myRootRef.childByAppendingPath("/products").setValue(product)
     }
     
     override func viewDidAppear(animated: Bool) {
         self.webView?.loadRequest(NSURLRequest(URL: NSURL(string: self.url!)!))
+        product.append(self.url!)
     }
 }
