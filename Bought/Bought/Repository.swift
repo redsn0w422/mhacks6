0e204c53c840d0e4ebb3ab4c88f0cc4dacad2903
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-09-12 01:24:18 -0400

    some work on table view setup

---
+++ Bought/Bought/Repository.swift
@@ -0,0 +1,9 @@
+//
+//  Repository.swift
+//  Bought
+//
+//  Created by Yasha Mostofi on 9/12/15.
+//  Copyright (c) 2015 Yasha Mostofi. All rights reserved.
+//
+
+import Foundation
--- Bought/Bought/ViewController.swift
+++ Bought/Bought/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var repositories = [String]()
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return repositories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = repositories[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
