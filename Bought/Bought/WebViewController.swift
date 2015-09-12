72e3a12d6bdb306af0ccedac85e2cc1fa882c9e1
Niraj Pant <nirajpant7@gmail.com>
2015-09-12 14:39:34 -0400

    Clean + Add Firebase

--- Bought/Bought/WebViewController.swift
+++ Bought/Bought/WebViewController.swift
@@ -8,46 +8,14 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class WebViewController: UIViewController {
     @IBOutlet weak var webView: UIWebView!
     
     var url:String?
     
-    func HTTPsendRequest(request: NSMutableURLRequest,callback: (String, String?) -> Void) {
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request,completionHandler :
-            {
-                data, response, error in
-                if error != nil {
-                    callback("", (error!.localizedDescription) as String)
-                } else {
-                    callback(NSString(data: data!, encoding: NSUTF8StringEncoding) as! String,nil)
-                }
-        })
-        
-        task.resume() //Tasks are called with .resume()
-        
-    }
-    
-    func httpGet(url: String, callback: (String, String?) -> Void) {
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!) //To get the URL of the receiver , var URL: NSURL? is used
-        HTTPsendRequest(request, callback: callback)
-    }
-    
     override func viewDidAppear(animated: Bool) {
-        self.webView?.loadRequest(NSURLRequest(URL: NSURL(string: self.url!)!))
-        /*
-        httpGet(self.url!) {
-            (data: String, error: String?) -> Void in
-            if error != nil {
-                print(error)
-            } else {
-                let myHTMLString: String! = data
-                self.webView.loadHTMLString(myHTMLString, baseURL: "http://www.apple.com/")
-            }
-        }
-*/
-        
+        self.webView?.loadRequest(NSURLRequest(URL: NSURL(string: self.url!)!))        
     }
 }
