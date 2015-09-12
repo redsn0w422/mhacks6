dade214c2248538754ce1854d71368ed817440b3
Niraj Pant <nirajpant7@gmail.com>
2015-09-12 05:15:28 -0400

    Webview working

--- Bought/Bought/WebViewController.swift
+++ Bought/Bought/WebViewController.swift
@@ -12,8 +12,37 @@ import UIKit
 class WebViewController: UIViewController {
     @IBOutlet weak var webView: UIWebView!
     
+    func HTTPsendRequest(request: NSMutableURLRequest,callback: (String, String?) -> Void) {
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request,completionHandler :
+            {
+                data, response, error in
+                if error != nil {
+                    callback("", (error!.localizedDescription) as String)
+                } else {
+                    callback(NSString(data: data!, encoding: NSUTF8StringEncoding) as! String,nil)
+                }
+        })
+        
+        task.resume() //Tasks are called with .resume()
+        
+    }
+    
+    func httpGet(url: String, callback: (String, String?) -> Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!) //To get the URL of the receiver , var URL: NSURL? is used
+        HTTPsendRequest(request, callback: callback)
+    }
+    
     override func viewDidAppear(animated: Bool) {
-        let myHTMLString: String! = "<h1>Hello Pizza!</h1>"
-        webView.loadHTMLString(myHTMLString, baseURL: nil)
+        httpGet("http://www.google.com") {
+            (data: String, error: String?) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                let myHTMLString: String! = data
+                self.webView.loadHTMLString(myHTMLString, baseURL: nil)
+            }
+        }
+        
     }
 }
